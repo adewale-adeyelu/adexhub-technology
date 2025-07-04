20448a2a3ee9b9f67e73a4bd771883495f0c12db
adewale-adeyelu <adewaleadeyelu17@gmail.comm>
2025-07-04 17:33:50 +0100

    updated

--- script.js
+++ script.js
@@ -72,3 +72,19 @@ function toggleAccordion(id) {
         icon.classList.replace("bi-chevron-down", "bi-chevron-up");
     }
 }
+
+// password
+document.getElementById('togglePassword').addEventListener('click', function() {
+    const passwordInput = document.getElementById('password');
+    const eyeIcon = document.getElementById('eyeIcon');
+    
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        eyeIcon.classList.remove('bi-eye-slash');
+        eyeIcon.classList.add('bi-eye');
+    } else {
+        passwordInput.type = 'password';
+        eyeIcon.classList.remove('bi-eye');
+        eyeIcon.classList.add('bi-eye-slash');
+    }
+});
